b3fcf23af55c7015efb7f61d3ab8efe2c53f9e8e
Mallikarjuna <spiderman05116@gmail.com>
2025-07-31 10:11:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,12 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors());
+app.use(cors({
+  origin: 'https://flight-booking-app-lovat.vercel.app/', // 🔁 replace with actual Vercel frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
+
 
 
 // ✅ Serve React build files
