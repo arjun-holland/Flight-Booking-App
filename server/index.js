8baff75bd732da5b1ea55c3eaba0df2e6a105737
Mallikarjuna <spiderman05116@gmail.com>
2025-07-31 10:45:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,8 +16,8 @@ app.use(express.json());
 app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors({
-  origin: 'https://flight-booking-app-lovat.vercel.app/', // 🔁 replace with actual Vercel frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  origin: 'https://flight-booking-app-lovat.vercel.app', // 🔁 replace with actual Vercel frontend URL
+ // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
 
