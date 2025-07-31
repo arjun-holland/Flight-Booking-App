6356e49f60f708ecf8158a1f1ff510c1fb0ee3fa
Mallikarjuna <spiderman05116@gmail.com>
2025-07-31 09:45:18 +0530

    Update GeneralContext.jsx

--- client/src/context/GeneralContext.jsx
+++ client/src/context/GeneralContext.jsx
@@ -21,7 +21,7 @@ const GeneralContextProvider = ({children}) => {
   const login = async () =>{
     try{
       const loginInputs = {email, password}
-        await axios.post('http://localhost:6001/login', loginInputs)
+        await axios.post('/login', loginInputs)
         .then( async (res)=>{
 
             localStorage.setItem('userId', res.data._id);
@@ -48,7 +48,7 @@ const GeneralContextProvider = ({children}) => {
   
   const register = async () =>{
     try{
-        await axios.post('http://localhost:6001/register', inputs)
+        await axios.post('/register', inputs)
         .then( async (res)=>{
             localStorage.setItem('userId', res.data._id);
             localStorage.setItem('userType', res.data.usertype);
