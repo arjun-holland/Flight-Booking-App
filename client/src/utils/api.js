836e64e5fe182e2f75f1636939eb6323e5149412
Mallikarjuna <spiderman05116@gmail.com>
2025-07-31 11:49:19 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -7,4 +7,13 @@ const API = axios.create({
   withCredentials: false,
 });
 
+// const API = axios.create({
+//   baseURL:
+//     process.env.NODE_ENV === 'development'
+//       ? 'http://localhost:6001'
+//       : process.env.REACT_APP_API_URL|| '',  //add the backend url as the value for the REACT_APP_API_URL in environmental variables
+//   withCredentials: false,
+// });
+
+
 export default API;
